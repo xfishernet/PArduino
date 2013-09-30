651cd162ccbaf8850df9b313a2e4372ea62a2491
xfishernet <xfisherbox@gmail.com>
2013-09-30 10:17:45 +0300

    some format text

--- service.js
+++ service.js
@@ -2,11 +2,11 @@ var Client = require('node-rest-client').Client;
 
 var SerialPort = require("serialport").SerialPort
 var serialport = require("serialport");
+
 var serialPort = new SerialPort("/dev/ttyUSB0", {
   baudrate: 115200,
   parser: serialport.parsers.readline("\n")
-}, true); // this is the openImmediately flag [default is true]
-
+}, true); 
 
 var client = new Client();
 
@@ -17,31 +17,28 @@ serialPort.open(function () {
   serialPort.on('data', function(data) {
     console.log('data received: ' + data);
 
-    obj = JSON.parse(data);
+     obj = JSON.parse(data);
 
-    
-args ={
+     args ={
 
-        headers:{"test-header":"client-api"},
-        data: {"pressure": obj.pressure, "temp": obj.temperature}
-      };
+	 	headers:{"test-header":"client-api"},
+        	data: {"pressure": obj.pressure, "temp": obj.temperature}
+           };
 
-	client.post("http://map.x-fisher.org.ua/meteo", args, function(data, response){
+     client.post("http://map.x-fisher.org.ua/meteo", args, function(data, response){
             
-	}).on('error', function(err) {
+    }).on('error', function(err) {
 		
-	    console.log("ERROR: " + err);
+      console.log("ERROR: " + err);
 
-	});
-
+    });
 
   });
 
-
 });
 
-
 var cronJob = require('cron').CronJob;
+
 new cronJob('00 */5 * * * *', function(){
 
     console.log('You will see this message every */5 second');
@@ -51,8 +48,4 @@ new cronJob('00 */5 * * * *', function(){
 	    console.log('results ' + results);
   });
 
-
-
 }, null, true, "America/Los_Angeles");
-
-
